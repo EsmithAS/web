7b5168035e6ccb6346159062cc2cabf4807606ac
consultorandroid2 <consultorandroid2@directoapp.pe>
2024-02-03 16:06:44 -0500

    web

--- src/app/api/post/route.js
+++ src/app/api/post/route.js
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { promises as fs } from 'fs';
-
+const domain = process.env.DOMAIN;
 
 
 export async function GET(request) {
-  const file = await fs.readFile(process.cwd() + '/dt.json', 'utf8');
+  const file = await fs.readFile( process.cwd() + '/src/app/api/dt.json', 'utf8');
 
   return Response.json({
     success: true,
@@ -14,7 +14,7 @@ export async function GET(request) {
 
 export async function POST(request) {
   const res = await request.json();
-  const file = await fs.readFile(process.cwd() + '/dt.json', 'utf8');
+  const file = await fs.readFile( process.cwd() + '/src/app/api/dt.json', 'utf8');
 
   let msg = JSON.parse(file);
   msg.push({
@@ -22,8 +22,7 @@ export async function POST(request) {
     date: new Date()    
   })
 
-  await fs.writeFile(process.cwd() + '/dt.json', JSON.stringify(msg), 'utf8');
-  console.log(res);
+  await fs.writeFile( process.cwd() + '/src/app/api/dt.json', JSON.stringify(msg), 'utf8');
   return Response.json({
     success: true,
     data: file,
