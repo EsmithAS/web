bfb41434e3f685d1a300a76e955ba32e6b4ac32f
consultorandroid2 <consultorandroid2@directoapp.pe>
2024-02-03 16:13:25 -0500

    web

--- src/app/api/post/route.js
+++ src/app/api/post/route.js
@@ -4,7 +4,7 @@ const domain = process.env.DOMAIN;
 
 
 export async function GET(request) {
-  const file = await fs.readFile( process.cwd() + '/src/app/api/dt.json', 'utf8');
+  const file = await fs.readFile( process.cwd() + '/dt.json', 'utf8');
 
   return Response.json({
     success: true,
@@ -14,7 +14,7 @@ export async function GET(request) {
 
 export async function POST(request) {
   const res = await request.json();
-  const file = await fs.readFile( process.cwd() + '/src/app/api/dt.json', 'utf8');
+  const file = await fs.readFile( process.cwd() + '/dt.json', 'utf8');
 
   let msg = JSON.parse(file);
   msg.push({
